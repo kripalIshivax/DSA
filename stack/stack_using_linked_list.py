8bd3a476a33f12ea8e965f66c07d657d6d4c94a5
Kripal Singh <kripalsingh@ishivax.com>
2026-04-03 11:40:11 +0530

    fixed stack implementation

--- stack/stack_using_linked_list.py
+++ stack/stack_using_linked_list.py
@@ -4,14 +4,41 @@ class Node:
         self.nxt = nxt
 class SLL:
     def __init__(self):
-        head = None
+        self.head = None
 
     def insert_at_start(self,val):
-        if head is None:
-            head = Node(val,None)
+        if self.head is None:
+            self.head = Node(val,None)
         else:
-           head =  Node(val,head)
-                
+           self.head =  Node(val,self.head)
 
-        
+    def delete_at_start(self):
+        if self.head.nxt is not None:
+            self.head = self.head.nxt
+    
+    def show(self):
+        temp = self.head
+        while temp.nxt is not None:
+            print(temp.val)
+            temp = temp.nxt
+        else:
+            print(temp.val)
+
+class Stack:
+    def __init__(self):
+        self.sll = SLL()
+    def push(self,item):
+        self.sll.insert_at_start(item)
+    def pop(self):
+        self.sll.delete_at_start()
+    def show(self):
+        self.sll.show()
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.show()   
+stack.pop()
+stack.show()
 
