a801d337de6be70deda98b3b2268b40d49b92c74
Kripal Singh <kripalsingh@ishivax.com>
2026-04-06 12:16:13 +0530

    added variations of insert methods

--- linked_list/doubly_linked_list.py
+++ linked_list/doubly_linked_list.py
@@ -27,10 +27,55 @@ class DoublyLinkedList:
 
     def insert_at_start(self,val):
         newNode = Node(None,val,self.head)
+        self.head.prev = newNode
+        self.head = newNode
+    
+    def insert_after_item(self,after_item,val):
+        temp = self.head
+        while temp.next is not None:
+            if temp.val == after_item:
+                newNode = Node(temp,val,temp.next)
+                backup = temp
+                temp.next = newNode
+                backup.next.prev = newNode
+                break
+            temp = temp.next
+        else:
+            if temp.val == after_item:
+                newNode = Node(temp,val,None)
+                temp.next = newNode
+            else:
+                print("No element found after last")
+
+   
+    def insert_before_item(self,before_item,val):
+        temp = self.head
+        while temp.next is not None:
+            if temp.val == before_item:
+                newNode = Node(temp.prev,val,temp)
+                backup = temp
+                temp.prev.next =  newNode
+                backup.prev = newNode
+                break
+            temp = temp.next
+        else:
+            if temp.val == before_item:
+                newNode = Node(temp.prev,val,temp)
+                backup = temp
+                temp.prev.next = newNode
+                backup.prev = newNode
+            else:
+                print("No element matched before last")
+
 
 d = DoublyLinkedList()
 d.insert(10)
 d.insert(20)
 d.insert(30)
 d.insert(40)
+d.printList()
+d.insert_after_item(30,35)
+d.insert_before_item(20,15)
+d.insert_after_item(33,32)
+d.insert_before_item(33,32)
 d.printList()
