7d6fe96c7cd907d3473ca0433d4f990872213e06
Kripal Singh <kripalsingh@ishivax.com>
2026-04-09 17:18:41 +0530

    numbers will print in increasing order now

--- recursion/print_first_n_natural_numbers.py
+++ recursion/print_first_n_natural_numbers.py
@@ -1,9 +1,9 @@
-def print_natural_nums(n):
-    if n<=0:
+def print_natural_nums(n,count):
+    if count>n:
         return
-    print(" ",n)
-    n-=1
-    print_natural_nums(n)
+    print(" ",count)
+    count+=1
+    print_natural_nums(n,count)
 n = int(input("please enter number"))    
-print_natural_nums(n)    
+print_natural_nums(n,1)    
 
